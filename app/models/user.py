43718c0f93ed2980192e7144417692e72463d24a
quentinnguyen123@gmail.com <quentinnguyen123@gmail.com>
2023-05-17 10:34:54 -0700

    removed trailing comma class User

--- app/models/user.py
+++ app/models/user.py
@@ -17,7 +17,7 @@ class User(db.Model, UserMixin):
     first_name = db.Column(db.String(40), nullable=False)
     last_name = db.Column(db.String(40), nullable=False)
     profile_pic = db.Column(db.String(255)
-                            # default=
+                            #, default=
                             )
 
     videos = db.relationship('Video', back_populates = 'user', cascade='all, delete-orphan')
