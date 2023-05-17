0276bef4ee2a164f6cf8b041485d53c8701e9771
quentinnguyen123@gmail.com <quentinnguyen123@gmail.com>
2023-05-17 12:08:30 -0700

    altered seeds/__init__.py

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -35,7 +35,7 @@ def seed_users_playlist_videos():
 # incrementing primary key, CASCADE deletes any dependent entities.  With
 # sqlite3 in development you need to instead use DELETE to remove all data and
 # it will reset the primary keys for you as well.
-def undo_users():
+def undo_users_playlist_videos():
     if environment == "production":
         db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
     else:
