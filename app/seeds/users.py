e58f62d3419950abc905dc867951aac3821a074a
quentinnguyen123@gmail.com <quentinnguyen123@gmail.com>
2023-05-17 12:53:08 -0700

    changed env.py

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -37,7 +37,7 @@ def seed_users_playlist_videos():
 # it will reset the primary keys for you as well.
 def undo_users_playlist_videos():
     if environment == "production":
-        db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
+        db.session.execute(f"TRUNCATE table {SCHEMA}.users_playlist_videos RESTART IDENTITY CASCADE;")
     else:
         db.session.execute(text("DELETE FROM users"))
 
