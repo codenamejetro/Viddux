813f9c2e0235c14a4e0e2e41de48d374ee0ecaaa
quentinnguyen123@gmail.com <quentinnguyen123@gmail.com>
2023-05-17 11:04:08 -0700

    added to_dict to class Video and class Playlist

--- app/models/playlist.py
+++ app/models/playlist.py
@@ -8,10 +8,10 @@ class Playlist(db.Model):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    public = db.Column(db.Boolean, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey(
         add_prefix_for_prod('users.id')), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    public = db.Column(db.Boolean, nullable=False)
     description = db.Column(db.String(255))
 
     created_at = db.Column(db.Date, nullable=False)
@@ -21,3 +21,14 @@ class Playlist(db.Model):
     user = db.relationship('User', back_populates='playlists')
     videos = db.relationship(
         'Video', secondary=playlist_videos, back_populates='playlists')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'name': self.name,
+            'public': self.public,
+            'description': self.description,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at
+        }
--- app/models/video.py
+++ app/models/video.py
@@ -10,6 +10,7 @@ class Video(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
     title = db.Column(db.String(100), nullable=False, unique=True)
+    mp4 = db.Column(db.String(255), nullable=False)
     preview_img = db.Column(db.String(255))
     description = description = db.Column(db.String(255))
     created_at = db.Column(db.Date, nullable=False)
@@ -18,3 +19,15 @@ class Video(db.Model):
     user = db.relationship('User', back_populates='videos')
     playlists = db.relationship(
         'Playlist', secondary=playlist_videos, back_populates='videos', cascade='all, delete')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'title': self.title,
+            'mp4': self.mp4,
+            'preview_img': self.preview_img,
+            'description': self.description,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at
+        }
