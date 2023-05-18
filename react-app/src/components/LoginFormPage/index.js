f544854cc7055484e79eea0e4d9134034c05f2ad
quentinnguyen123@gmail.com <quentinnguyen123@gmail.com>
2023-05-18 08:35:16 -0700

    login and signup pages skeleton completed

--- react-app/src/components/LoginFormPage/index.js
+++ react-app/src/components/LoginFormPage/index.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-import { login } from "../../store/session";
+import { login, signUp } from "../../store/session";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
 import './LoginForm.css';
+import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
 
 function LoginFormPage() {
   const dispatch = useDispatch();
@@ -21,35 +22,53 @@ function LoginFormPage() {
     }
   };
 
+  const demoHandler = () => {
+    console.log('hi')
+    dispatch(login('demo@aa.io', 'password'))
+  }
+
   return (
     <>
-      <h1>Log In</h1>
-      <form onSubmit={handleSubmit}>
-        <ul>
-          {errors.map((error, idx) => (
-            <li key={idx}>{error}</li>
-          ))}
-        </ul>
-        <label>
-          Email
-          <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </label>
-        <label>
-          Password
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </label>
-        <button type="submit">Log In</button>
-      </form>
+      <div className="login-wrapper">
+        <div className="login-wrapper-inner">
+          <h1>Welcome back!</h1>
+          <form onSubmit={handleSubmit} className="login-form">
+            <div className="login-errors-div">
+              {errors.map((error, idx) => (
+                <li key={idx}>{error}</li>
+              ))}
+
+            </div>
+            {/* <div></div> */}
+            <input
+              className="login-email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              password='Enter your email'
+            />
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Enter your password"
+            />
+
+            <div className="login-bottom">
+              <div className="login-bottom-top">
+                <button type="submit">Sign in</button>
+              </div>
+              <div className="login-bottom-bottom">
+                <NavLink className='login-signup' to='/signup'>
+                  Don't have an account?
+                </NavLink>
+                <div onClick={demoHandler}>Demo</div>
+              </div>
+            </div>
+          </form>
+
+        </div>
+      </div>
     </>
   );
 }
